98589ee793fec193bbae22dd37a874889a203f0f
dvgodoy <dvgodoy@gmail.com>
2018-10-10 22:47:19 +0200

    schema test

--- tests/handyspark/sql/test_schema.py
+++ tests/handyspark/sql/test_schema.py
@@ -1,10 +1,10 @@
+import numpy as np
 import numpy.testing as npt
 import handyspark
 from handyspark.sql.schema import generate_schema
 
 def test_generate_schema(sdf):
     res = sdf.schema
-    hres = generate_schema(sdf.columns, sdf.toPandas().dtypes.values)
-    print(res)
-    print(hres)
+    hres = generate_schema(sdf.columns, [np.int32, np.int32, np.int32, str, str, np.float64, np.int32, np.int32, str,
+                                         np.float64, str, str])
     npt.assert_array_equal(hres, res)
